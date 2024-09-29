eb1d33b16fa8a4099d4c026058fd1303fd25d9dc
Mukesh Prajapati <mukeshprajapat7348@gmail.com>
2024-09-30 00:42:22 +0530

    getBlogs API completed

--- API/controllers/blog.controller.js
+++ API/controllers/blog.controller.js
@@ -36,4 +36,43 @@ const createBlog = async (req, res, next) => {
   }
 };
 
-export { createBlog };
+const getBlogs = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const blogLimit = parseInt(req.query.limit) || 9;
+    const sortBy = req.query.sortBy === "asc" ? 1 : -1;
+    const blogs = await Blog.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.blogId && { _id: req.query.blogId }),
+      ...(req.query.searchVal && {
+        $or: [
+          { title: { $regex: req.query.searchVal, $options: "i" } },
+          { content: { $regex: req.query.searchVal, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortBy })
+      .skip(startIndex)
+      .limit(blogLimit);
+
+    const totalBlogCount = await Blog.countDocuments();
+    const now = new Date();
+    const lastMonth = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthBlogCount = await Blog.countDocuments({
+      createdAt: { $gte: lastMonth },
+    });
+
+    return res.status(200).json({ blogs, totalBlogCount, lastMonthBlogCount });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { createBlog, getBlogs };
--- API/routes/Blog.routes.js
+++ API/routes/Blog.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createBlog } from "../controllers/blog.controller.js";
+import { createBlog, getBlogs } from "../controllers/blog.controller.js";
 import { verifyToken } from "../utils/user.verification.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createBlog);
+router.get("/getBlogs", getBlogs);
 
 export default router;
