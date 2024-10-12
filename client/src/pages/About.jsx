1b40bb9c92d90c6d7bd62cb9835d802b98c73989
Mukesh Prajapati <mukeshprajapat7348@gmail.com>
2024-10-12 18:15:40 +0530

    added about and projects funtionality

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
-    return (
-        <div>About</div>
-    )
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="max-w-2xl p-5 border border-teal-500 rounded-md rounded-tl-3xl rounded-br-3xl">
+        <h1 className="text-3xl font-semibold my-2">About Mukesh's Blog</h1>
+        <div className="text-gray-500 flex flex-col gap-6 text-md">
+          <p>
+            This blog application was created by Mukesh Prajapati as a personal
+            project to showcase his web development skills.
+          </p>
+          <p>
+            He is a passionate full-stack developer, he brings expertise in both
+            front-end and back-end technologies, enabling to build complete and
+            scalable web applications. His passion for programming languages and
+            technology trends reflects in his blog content, where he shares
+            insightful knowledge, tips, and tutorials of the developers.
+          </p>
+          <p>
+            He documents a blog about personal or professional projects,
+            explaining the stack used (e.g., MongoDB, Express, React, Node.js
+            (MERN)). Include code snippets, challenges faced, and solutions
+            implemented.
+          </p>
+          <p>
+            Whether it's exploring the latest advancements in JavaScript
+            frameworks, cloud computing, or AI integration, his ability to break
+            down complex topics into digestible content helps fellow developers
+            stay informed and improve their skills.
+          </p>
+          <p>
+            He focuses on creating in-depth articles or tutorials on both
+            established and emerging technologies. For example, a series on best
+            practices for Angular or React or Node.js or Sprint boot, Java, or
+            guides on microservices architecture can position you as an expert.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import React from "react";
 
 export default function Projects() {
-    return (
-        <div>Projects</div>
-    )
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-4xl flex flex-wrap justify-center gap-10">
+        <div className="w-96 border border-teal-500 hover:border-2 p-5 rounded-md flex flex-col gap-4 hover:scale-110 transition-all duration-300">
+          <h1 className="text-2xl">
+            ListLogic{" "}
+            <a
+              href="https://github.com/MukeshPrajapati7348/Daily-Task-Manager"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ml-2 text-xs hover:underline text-teal-500"
+            >
+              github{" "}
+            </a>
+          </h1>
+          <p className="text-gray-500">
+            A To-Do Application helps users manage and organize tasks
+            efficiently by allowing them to create, prioritize, and track tasks.
+            It enhances productivity through features like task categorization,
+            reminders, and syncing, making it ideal for personal and
+            professional use.
+          </p>
+        </div>
+        <div className="w-96 border border-teal-500 hover:border-2 p-5 rounded-md flex flex-col gap-4 hover:scale-110 transition-all duration-300">
+          <h1 className="text-2xl">
+            ClearSkies{" "}
+            <a
+              href="https://github.com/MukeshPrajapati7348/My-weather-app"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ml-2 text-xs hover:underline text-teal-500"
+            >
+              github{" "}
+            </a>
+          </h1>
+          <p className="text-gray-500">
+            A Weather Web Application provides real-time weather updates,
+            forecasts and typically displays current weather conditions,
+            short-term and long-term forecasts.
+            {/* , allowing users to stay informed about
+          conditions. With features like hourly updates and interactive maps, it
+          helps users plan activities and stay prepared for weather changes. */}
+          </p>
+        </div>
+        <div className="w-96 border border-teal-500 hover:border-2 p-5 rounded-md flex flex-col gap-4 hover:scale-110 transition-all duration-300">
+          <h1 className="text-2xl">
+            Portfolio{" "}
+            <a
+              href="https://github.com/MukeshPrajapati7348/My-New-Portfolio-"
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ml-2 text-xs hover:underline text-teal-500"
+            >
+              github{" "}
+            </a>
+          </h1>
+          <p className="text-gray-500">
+            A Portfolio Web Application showcases my professional work, skills,
+            and achievements in a visually appealing and interactive format. It
+            typically includes sections for personal information, project
+            highlights, work experience, technical skills, and contact details.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
